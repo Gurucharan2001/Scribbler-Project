3d31a16253a066907210d382c7e3182dc83b5839
Gurucharan2001 <togmaparu@gmail.com>
2021-03-28 09:21:15 +0530

    Complete the edit and save functionality

--- scripts/post.js
+++ scripts/post.js
@@ -1,14 +1,45 @@
 //Edit button 
 $(document).ready(function(){
     $(".edit-btn").click(function(){
-        $("#edit-btn").html("Save  <i class='fa fa-save'></i>")
-        var edit_box = $("<textarea cols='200' rows='13'></textarea>")
-        edit_box.attr("id","edited-text-box")
+        $("#edit-btn").css("display","none")
+        $("#save-btn").css("display","block")
+        var edit_content = $("<textarea cols='200' rows='13'></textarea>")
+        edit_content.attr("id","edited-text-box")
         var text_post = $("#blog-text").html()
-        edit_box.html(text_post)
-        $("#post-text1").append(edit_box)
+        edit_content.html(text_post)
+        $("#post-text1").append(edit_content)
+        
+        var edit_title = $("<textarea cols='30' rows='1' style='font-size:18px;'></textarea>")
+        var post_title = $("#post-title").html()
+        edit_title.attr("id","edited-title")
+        edit_title.html(post_title)
+        $("#main-box").append(edit_title)
+        $("#post-title").css("display","none")
+        edit_title.css("margin-left","40%")
+
+
         $("#blog-text").css("display","none")
         $("#edit-btn").css("border","none")
+    })
+
+    //save the new post
+    $("#save-btn").click(function(){
+        $("#edit-btn").css("display","block")
+        $("#save-btn").css("display","none")
+
+        $("#blog-text").html()
+        $("#post-title").html()
+
+
+        var new_post = $("textarea#edited-text-box").val()
+        var edited_title = $("textarea#edited-title").val()
+        $("#blog-text").css("display","block")
+        $("#blog-text").html(new_post)
+
+        $("#post-title").css("display","block")
+        $("#post-title").html(edited_title)
+        $("#edited-title").css("display","none")
+        $("#edited-text-box").css("display","none")
     })
 
     //like button
